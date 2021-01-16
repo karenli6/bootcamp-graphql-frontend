d676b898ce0f6d703bf90b8fa46489d63c9551c9
Karen Li <karennli49@gmail.com>
2021-01-15 23:30:47 -0800

    updating graphql practice

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,78 +1,116 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import {
-  ALLAUTHORS, ADDAUTHORS, UPDATEAUTHORS, GETAUTHORBOOKS,
+  ALLAUTHORS, ADDAUTHORS, UPDATEAUTHORS,
 } from './graphql'
 
 
 const Home = () => {
-  const { data, loading, error } = useQuery(ALLAUTHORS)
+  // REACT
+  // input form - was having some issues with useReducer here :(
+  const [idval, setId] = useState('')
 
-  // adding author
-  const [addAuthor, { error: addAuthorError, loading: addAuthorLoading }] = useMutation(ADDAUTHORS,
-    {
-      update: (client, { authordata }) => {
-        try {
-          const temp = client.readQuery({ query: ALLAUTHORS })
-          temp.allAuthors = [...temp.allAuthors, authordata.createAuthor]
+  const [first, setFirst] = useState('')
+  const [last, setLast] = useState('')
+  const [ageNum, setAgeNum] = useState('')
+  const [emailAdd, setEmailAdd] = useState('')
+  const [books, setBooks] = useState(0)
+  const [address, setAddress] = useState('')
+  const resetAll = () => {
+    setId('')
+    setFirst('')
+    setLast('')
+    setAgeNum('')
+    setEmailAdd('')
+    setBooks('')
+    setAddress('')
+  }
+  const handleChangeId = element => {
+    setId(element.target.value)
+  }
 
-          client.writeQuery({ query: ALLAUTHORS, temp })
-        } catch (err) {
-          console.log(err)
-          throw new Error('update failed')
-        }
-      },
+  const handleChangeFirst = element => {
+    setFirst(element.target.value)
+  }
+
+  const handleChangeLast = element => {
+    setLast(element.target.value)
+  }
+
+  const handleChangeAge = element => {
+    setAgeNum(element.target.value)
+  }
+
+  const handleChangeEmail = element => {
+    setEmailAdd(element.target.value)
+  }
+
+  const handleChangeBooks = element => {
+    setBooks(element.target.value)
+  }
+
+  const handleChangeAddress = element => {
+    setAddress(element.target.value)
+  }
+
+
+  // REACT
+
+
+  const {
+    data, refetch: refetchAuthors, loading, error,
+  } = useQuery(ALLAUTHORS)
+
+
+  // updating an author
+  const [updateAuthor] = useMutation(UPDATEAUTHORS)
+
+  const editAuthor = async () => {
+    await updateAuthor({
       variables: {
+        id: idval,
         input: {
-          firstName: 'version2.0',
-          lastName: 'testing',
-          email: 'testing@gmail.com',
-          age: 40,
-          numBooksPublished: 2,
+          firstName: first,
+          lastName: last,
+          email: emailAdd,
+          // eslint-disable-next-line radix
+          age: parseInt(ageNum),
+          numBooksPublished: books,
+          addressId: address,
         },
       },
-      refetchQueries: () => [{ query: ALLAUTHORS }],
+
     })
 
-  // display author books
-  //   const { data: authorBooks, loading: authorBooksLoading, error: authorBooksError } = useQuery(GETAUTHORBOOKS)
-
-
-  // update author
-  //   const [updateAuthor, { error: updateAuthorError, loading: updateAuthorLoading }] = useMutation(UPDATEAUTHORS,
-  //     {
-  //       variables: {
-  //         id: '9a4d851b-7c82-4954-8524-590b6ddd1153',
-  //         input: {
-  //           firstName: 'KarenKaren',
-  //           lastName: 'Liiiiiii',
-  //           email: 'karen5.0@gmail.com',
-  //           //   age: 100,
-  //           //   numBooksPublished: 2,
-  //         },
-  //       },
-  //       refetchQueries: () => [{ query: ALLAUTHORS }],
-  //     })
-
-  // display Author's books
-
-  // display Publisher's books
-
-  // error printing
-  // if (updateAuthorError) {
-  //   console.log(updateAuthorError)
-  //   return <p> There was an error with updating an Author</p>
-  // }
-  if (addAuthorError) {
-    console.log(addAuthorError)
-
-    return <p> There was an error with adding an Author</p>
+    resetAll()
+    refetchAuthors()
   }
-  //   if (authorBooksError) {
-  //     console.log(authorBooksError)
 
-  //     return <p> There was an error with getting Author books</p>
-  //   }
+
+  // adding new author
+  const [addAuthor] = useMutation(ADDAUTHORS)
+
+  const createNewAuthor = async () => {
+    await addAuthor({
+      variables: {
+        input: {
+          firstName: first,
+          lastName: last,
+          email: emailAdd,
+          // eslint-disable-next-line radix
+          age: parseInt(ageNum),
+          numBooksPublished: books,
+          addressId: address,
+        },
+      },
+
+    })
+
+    resetAll()
+    refetchAuthors()
+  }
+
+
   if (error) {
     console.log(error)
     return <p> ERROR occurred with Allauthors</p>
@@ -81,20 +119,62 @@ const Home = () => {
 
   return (
     <>
-      <button type="button" onClick={addAuthor}>Add Me</button>
 
-      {loading || addAuthorLoading ? 'loading...' : data.allAuthors.map(author => (
+      <div>
+      Welcome to the Directory of Authors
+      </div>
+      <div>
+        <p>Input information for author:</p>
 
-        <>
+        <form>
+          {/* <label>First Name</label> */}
+          {/* <input type="text" value={first} onChange={handleChangeFirst} /> */}
+          <p>ID</p>
+          <input type="text" value={idval} onChange={handleChangeId} />
+          <br />
+          <p>First Name</p>
+          <input type="text" value={first} onChange={handleChangeFirst} />
+          <br />
+          <p>Last Name</p>
+          <input type="text" value={last} onChange={handleChangeLast} />
+          <br />
+          <p>Email</p>
+          <input type="text" value={emailAdd} onChange={handleChangeEmail} />
+          <br />
+          <p>Age</p>
+          <input type="text" value={ageNum} onChange={handleChangeAge} />
+          <br />
+          <p>Number of Books Published</p>
+          <input type="text" value={books} onChange={handleChangeBooks} />
+          <br />
+          <p>Address ID</p>
+          <input type="text" value={address} onChange={handleChangeAddress} />
+          <br />
+
+
+          <button type="button" onClick={createNewAuthor}>ADD new author</button>
+          <br/>
+          <button type="button" onClick={editAuthor}>UPDATE this author</button>
+
+        </form>
+
+      </div>
+
+
+      {loading ? 'loading...'
+
+        : data.allAuthors.map(author => (
+
+          <>
           <p>
-            {' '}
-            {author.firstName}
-            {' '}
-            {author.lastName}
-          </p>
+              {' '}
+              {author.firstName}
+              {' '}
+              {author.lastName}
+            </p>
 
         </>
-      ))}
+        ))}
     </>
   )
 }
